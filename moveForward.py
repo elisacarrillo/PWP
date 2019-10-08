b00bea21df2c4c7f8e69da974ef4361acd6bd14b
elisacarrillo <46950428+elisacarrillo@users.noreply.github.com>
2019-10-08 08:46:44 -0400

    Update moveForward.py

--- moveForward.py
+++ moveForward.py
@@ -2,51 +2,75 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.cleanup()
-
-# ///////////////// Define Motor Driver GPIO Pins /////////////////
-# Motor A, RIGHT Side GPIO CONSTANTS
-ENA = 21  # ENA - H-Bridge enable pin
-IN1 = 26  # IN1 - Forward Drive
-IN2 = 19  # IN2 - Reverse Drive
-# Motor B, LEFT Side GPIO CONSTANTS
-ENB = 5  # ENB - H-Bridge enable pin
-IN3 = 13  # IN1 - Forward Drive
-IN4 = 20  # IN2 - Reverse Drive
-
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ENA, GPIO.OUT)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(ENB, GPIO.OUT)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
-
-GPIO.output(IN1,GPIO.LOW)
-GPIO.output(IN2,GPIO.LOW)
-GPIO.output(IN3,GPIO.LOW)
-GPIO.output(IN4,GPIO.LOW)
-
-p = GPIO.PWM(ENA,7)
-p2 = GPIO.PWM(ENB,7)
-
-p.start(54)
-p2.start(45)
 
 class RobotMotion:
-    def __init__(self, time):
-        self.time = time
-    def forward(self):
-        GPIO.output(IN1,GPIO.HIGH)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW)
-        sleep(self.time)
+    def __init__(self):
+        # Motor A, RIGHT Side GPIO CONSTANTS
+        self.ENA = 21  # ENA - H-Bridge enable pin
+        self.IN1 = 26  # IN1 - Forward Drive
+        self.IN2 = 19  # IN2 - Reverse Drive
+        # Motor B, LEFT Side GPIO CONSTANTS
+        self.ENB = 5  # ENB - H-Bridge enable pin
+        self.IN3 = 13  # IN1 - Forward Drive
+        self.IN4 = 20  # IN2 - Reverse Drive
 
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.ENA, GPIO.OUT)
+        GPIO.setup(self.IN1, GPIO.OUT)
+        GPIO.setup(self.IN2, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
 
-twoForward = RobotMotion(1.95)
-twoForward.forward()
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+        self.rightMotor = GPIO.PWM(self.ENA, 6.75)
+        self.leftMotor = GPIO.PWM(self.ENB, 6.75)
+
+    def forward(self, time):
+        self.rightMotor.start(54)
+        self.leftMotor.start(45)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+        sleep(time)
+
+    def right(self, time):
+        self.rightMotor.start(54)
+        self.leftMotor.start(45)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+        sleep(time)
+    def backwards(self,time):
+        self.rightMotor.start(54)
+        self.leftMotor.start(45)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+        sleep(time)
+
+    def wait(self, time):
+        self.rightMotor.start(0)
+        self.leftMotor.start(0)
+        sleep(time)
 
 GPIO.cleanup()
+AA3000 = RobotMotion()
+AA3000.forward(1.65)
+AA3000.wait(.6)
+AA3000.right(.5)
+AA3000.wait(.6)
+AA3000.forward(1.7)
+AA3000.wait(.6)
+AA3000.backwards(1)
+
+GPIO.cleanup()
+
+
