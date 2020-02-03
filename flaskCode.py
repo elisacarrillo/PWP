c00b047849612db579d7d41c40cde5a41008164f
elisacarrillo <46950428+elisacarrillo@users.noreply.github.com>
2020-02-03 08:51:53 -0500

    Update flaskCode.py

--- flaskCode.py
+++ flaskCode.py
@@ -1,9 +1,15 @@
 from robotMotion import RobotMotion
-from flask import Flask
+from flask import Flask, request, send_from_directory
+from picamera import PiCamera
+from time import sleep
 #from communication import x
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '')
 AA3000 = RobotMotion()
 VERSION = 1.0
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 @app.route('/fwd/<int:x>')
 def front(x):
