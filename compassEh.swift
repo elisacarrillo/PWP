20e1340a209deac46d0754e87aaa677e61fd95aa
elisacarrillo <46950428+elisacarrillo@users.noreply.github.com>
2020-02-13 08:25:38 -0500

    Add files via upload

---
+++ compassEh.swift
@@ -0,0 +1,12 @@
+//
+//  compassEh.swift
+//  
+//
+//  Created by local on 2/12/20.
+//
+
+import UIKit
+
+class compassEh: NSObject {
+
+}
--- flaskCode.py
+++ flaskCode.py
@@ -1,40 +1,45 @@
 from robotMotion import RobotMotion
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, send_file
 from picamera import PiCamera
+
 from time import sleep
 #from communication import x
-app = Flask(__name__, static_url_path = '')
+app = Flask(__name__)
+
 AA3000 = RobotMotion()
 VERSION = 1.0
 
-@app.route('/')
-def root():
+@app.route('/api/')
+def gui():
+    #return "instruction recieved"
     return app.send_static_file('index.html')
+
 
 @app.route('/fwd/<int:x>')
 def front(x):
 
     AA3000.forward(x)
-    return "DONE\n"
+    return "Done Moving \n"
     GPIO.cleanup()
 
 @app.route('/bwd/<int:x>')
 def back(x):
 
     AA3000.backwards(x)
-    return "DONE\n"
+    return "Done Moving \n"
     GPIO.cleanup()
 @app.route('/lt/<int:x>')
 def left(x):
 
     AA3000.right(x)
-    return "DONE\n"
+    return "Done Moving \n"
     GPIO.cleanup()
 @app.route('/rt/<int:x>')
 def right(x):
 
     AA3000.backright(x)
-    return "DONE\n"
+    return "Done Moving \n"
     GPIO.cleanup()
+
 if __name__ == "__main__":
     app.run(debug=True, port = 8080, host= '192.168.1.108')
